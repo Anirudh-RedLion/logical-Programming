72565d8d6faf4031fea647e45e692f6e01aef2d9
Anirudh-RedLion <87081906+Anirudh-RedLion@users.noreply.github.com>
2022-05-29 14:58:13 +0530

    Add files via upload
    Initial commit

---
+++ ani-programming-practice/src/main/java/com/anidis/logical/AniProgrammingPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.anidis.logical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniProgrammingPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AniProgrammingPracticeApplication.class, args);
+	}
+
+}
---
+++ ani-programming-practice/src/test/java/com/anidis/logical/AniProgrammingPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anidis.logical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniProgrammingPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
