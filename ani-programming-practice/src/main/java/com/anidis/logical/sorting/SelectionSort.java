4aceb49a4dd5276b2d449510e01960dabf65e8d2
Ani <anirudhdelhi008@gmail.com>
2022-05-30 20:51:01 +0530

    Selection Sort

--- ani-programming-practice/src/main/java/com/anidis/logical/sorting/SelectionSort.java
+++ ani-programming-practice/src/main/java/com/anidis/logical/sorting/SelectionSort.java
@@ -4,23 +4,31 @@ public class SelectionSort {
 
 	public static void main(String args[]) {
 
-		int input[] = { 5, 9, 4, 6, 2, 1, 3, 8, 7 };
+		int input[] = { 5, 9, 4, 6, 12, 1, 3, 8, 7,0 ,2};
 		printArray(input);
 		System.out.println("--------------------------");
 		selectionSort(input);
+		printArray(input);
 	}
 
 	private static void selectionSort(int[] input) {
 		// TODO Auto-generated method stub
 
 		// 1st loop to traverse all element
-		for (int i = 0; i <= input.length; i++) {
+		for (int i = 0; i < input.length-1; i++) {
 
+			int smallest = i;
 			// 2nd loop to select small element
 			for (int j = i + 1; j < input.length; j++) {
 
+				if (input[smallest] > input[j]) {
+					smallest = j;
+				}
 			}
-
+			int temp = input[i];
+			input[i] = input[smallest];
+			input[smallest] = temp;
+			printArray(input);
 		}
 
 	}
