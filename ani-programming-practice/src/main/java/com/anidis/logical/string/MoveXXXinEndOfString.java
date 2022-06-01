b790caa5d867d5619fadc2e0d7b64accde8b1664
Ani <anirudhdelhi008@gmail.com>
2022-06-01 18:01:09 +0530

    StringSwift

---
+++ ani-programming-practice/src/main/java/com/anidis/logical/string/MoveXXXinEndOfString.java
@@ -0,0 +1,37 @@
+package com.anidis.logical.string;
+
+public class MoveXXXinEndOfString {
+
+	public static void main(String args[]) {
+
+		String input = "axbcxxd";
+		String selectedText = "x";
+		char ch = 'x';
+		Character cch = Character.valueOf('x');
+		// expected : abcdxxx
+		System.out.println("Input = "+input);
+		String output = shiftChracter(input, selectedText, ch, cch);
+
+		System.out.println(output);
+
+	}
+
+	private static String shiftChracter(String input, String selectedText, char ch, Character cch) {
+
+		StringBuilder nonselected = new StringBuilder();
+		StringBuilder selected = new StringBuilder();
+		for (int i = 0; i < input.length(); i++) {
+
+			System.out.println(" a: " + input.charAt(i) + " | " + ch+" == "+cch);
+			if (ch == input.charAt(i)) {
+				selected.append(input.charAt(i));
+			} else
+				nonselected.append(input.charAt(i));
+
+		}
+
+		return nonselected.toString() + selected.toString();
+
+	}
+
+}
