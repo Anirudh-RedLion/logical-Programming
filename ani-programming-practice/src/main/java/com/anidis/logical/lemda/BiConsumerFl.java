da5bbeab39929fabdb7a8c1fa14f478687ab3779
Ani <anirudhdelhi008@gmail.com>
2022-06-03 23:48:01 +0530

    BiConsumer

--- ani-programming-practice/src/main/java/com/anidis/logical/lemda/BiConsumerFl.java
+++ ani-programming-practice/src/main/java/com/anidis/logical/lemda/BiConsumerFl.java
@@ -1,5 +1,25 @@
 package com.anidis.logical.lemda;
 
+import java.util.function.BiConsumer;
+
+import com.anidis.logical.lemda.sampledata.Functinal_Java8;
+import com.anidis.logical.lemda.sampledata.Student;
+
+//11:37 -47
 public class BiConsumerFl {
+
+	public static void main(String ani[]) {
+
+		// Consumer
+		BiConsumer<Student, String> studentConsumer = (s, greet) -> {
+
+			System.out.println(greet + " " + s.getName());
+
+		};
+
+		System.out.println("-------- OUTPut --------");
+		Functinal_Java8.getAllStudents().forEach(s -> studentConsumer.accept(s, "Hello"));
+
+	}
 
 }
